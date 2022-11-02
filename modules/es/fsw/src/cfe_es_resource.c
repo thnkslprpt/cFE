7adbef4d9ffe61c2dd5b93668384b359497c2d97
Avi <thnkslprpt@gmail.com>
2022-11-02 14:27:18 +1000

    test

--- modules/es/fsw/src/cfe_es_resource.c
+++ modules/es/fsw/src/cfe_es_resource.c
@@ -85,21 +85,15 @@ CFE_ES_AppRecord_t *CFE_ES_LocateAppRecordByName(const char *Name)
     /*
     ** Search the Application table for an app with a matching name.
     */
-    Count     = CFE_PLATFORM_ES_MAX_APPLICATIONS;
-    while (true)
+    for (Count = CFE_PLATFORM_ES_MAX_APPLICATIONS;
+         !(CFE_ES_AppRecordIsUsed(AppRecPtr) && strcmp(Name, CFE_ES_AppRecordGetName(AppRecPtr)) == 0);
+         ++AppRecPtr, --Count)
     {
         if (Count == 0)
         {
             AppRecPtr = NULL;
             break;
         }
-        if (CFE_ES_AppRecordIsUsed(AppRecPtr) && strcmp(Name, CFE_ES_AppRecordGetName(AppRecPtr)) == 0)
-        {
-            break;
-        }
-
-        ++AppRecPtr;
-        --Count;
     }
 
     return AppRecPtr;
