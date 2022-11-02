bb293eb0d776ee4c5eaa46529812920762e9effe
Avi <thnkslprpt@gmail.com>
2022-11-02 14:16:48 +1000

    test

--- modules/es/fsw/src/cfe_es_resource.c
+++ modules/es/fsw/src/cfe_es_resource.c
@@ -79,27 +79,21 @@ CFE_ES_TaskId_t CFE_ES_TaskId_FromOSAL(osal_id_t id)
  *-----------------------------------------------------------------*/
 CFE_ES_AppRecord_t *CFE_ES_LocateAppRecordByName(const char *Name)
 {
-    CFE_ES_AppRecord_t *AppRecPtr;
+    CFE_ES_AppRecord_t *AppRecPtr = CFE_ES_Global.AppTable;
     uint32              Count;
 
     /*
     ** Search the Application table for an app with a matching name.
     */
-    AppRecPtr = CFE_ES_Global.AppTable;
-    Count     = CFE_PLATFORM_ES_MAX_APPLICATIONS;
-    while (true)
+    for (Count = CFE_PLATFORM_ES_MAX_APPLICATIONS;
+         (CFE_ES_AppRecordIsUsed(AppRecPtr) && strcmp(Name, CFE_ES_AppRecordGetName(AppRecPtr)) == 0);)
     {
         if (Count == 0)
         {
             AppRecPtr = NULL;
             break;
         }
-        if (CFE_ES_AppRecordIsUsed(AppRecPtr) && strcmp(Name, CFE_ES_AppRecordGetName(AppRecPtr)) == 0)
-        {
-            break;
-        }
-
-        AppRecPtr++;
+        ++AppRecPtr;
         --Count;
     }
 
