f1129a7b4881539ea38d77fedc546e9e35298578
Avi Weiss <thnkslprpt@gmail.com>
2025-07-11 07:36:46 +0200

    test

--- modules/es/ut-coverage/es_UT.c
+++ modules/es/ut-coverage/es_UT.c
@@ -2450,7 +2450,7 @@ void TestGenericPool(void)
     CFE_UtAssert_SETUP(ES_UT_PoolDirectRetrieve(&Pool1, Offset1 - CFE_ES_GENERIC_POOL_DESCRIPTOR_SIZE, &BdPtr));
     BdPtr->CheckBits = ~((uint16)BdPtr->ActualSize);
     UtAssert_INT32_EQ(CFE_ES_GenPoolPutBlock(&Pool1, &BlockSize, Offset1), CFE_ES_POOL_BLOCK_INVALID);
-    BdPtr->CheckBits  = CFE_ES_CHECK_PATTERN;
+    BdPtr->CheckBits  = ~((uint16)BdPtr->ActualSize);
     BdPtr->ActualSize = 0;
     UtAssert_INT32_EQ(CFE_ES_GenPoolPutBlock(&Pool1, &BlockSize, Offset1), CFE_ES_POOL_BLOCK_INVALID);
 
